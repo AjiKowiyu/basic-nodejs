cbce3ab2119fb8be176af32d194f74e2ca3f5768
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2023-05-24 21:49:31 +0700

    sesi1: variabel, operator mtk, operator logika

---
+++ sesi1-operator-logika.js
@@ -0,0 +1,5 @@
+var gaji_aziz = 497000
+var gaji_nur = 497000
+
+//cek perbandingan
+console.log( gaji_aziz >= gaji_nur )
---
+++ sesi1-operator-mtk.js
@@ -0,0 +1,14 @@
+var gaji = 4900000
+var lembur = 250000
+
+// operasi penambahan
+console.log( (gaji + lembur).toLocaleString() )
+
+// operasi pengurangan
+console.log( (gaji - lembur).toString() )
+
+// operasi perkalian
+console.log( (gaji * lembur).toLocaleString('id-ID') )
+
+// operasi pembagian
+console.log( gaji / lembur )
