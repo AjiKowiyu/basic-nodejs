386e091e4e2a8ad223831170c87a2b96d8232605
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2023-05-31 21:35:19 +0700

    sesi2: perkondisian switch case

---
+++ sesi2-perkondisian-switchcase.js
@@ -0,0 +1,16 @@
+var pesanan = 'air mineral'
+
+switch (pesanan) {
+    case 'kopi':
+        console.log('anda pesan kopi')
+        break;
+    case 'teh':
+        console.log('anda pesan teh')
+        break;
+    case 'susu':
+        console.log('anda pesan susu')
+        break;
+    default:
+        console.log('tidak ada di menu')
+        break;
+}
