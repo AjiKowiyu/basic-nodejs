c3ada8ceed819b27ea0aee0546d652fa8b04aa4d
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2023-06-02 19:58:25 +0700

    sesi3: perulangan (looping)

---
+++ sesi3-perulangan-bercabang.js
@@ -0,0 +1,6 @@
+for (var i=1; i <= 12; i++) {
+    console.log('Siswa ke ' + i)
+    for (var j=0; j <= 12; j++) {
+        console.log('Data Profil ke ' + j)
+    }
+}
---
+++ sesi3-perulangan-for.js
@@ -0,0 +1,5 @@
+
+// (mulai; batas; jenis)
+for (var i=1; i <= 12; i++) {
+    console.log('Siswa ke ' + i)
+}
