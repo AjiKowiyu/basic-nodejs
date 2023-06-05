76a91efeb2a14c292c91925d63b90ffc06173dbb
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2023-06-05 20:39:51 +0700

    sesi4: var let const

---
+++ sesi4-var-let-const.js
@@ -0,0 +1,15 @@
+// var:
+// 1. (+) nama variabel bisa di deklarasi ulang
+// 2. (+) nilai variabel bisa di timpa/ubah
+
+// let:
+// 1. (-) nama variabel tidak bisa di deklarasi ulang
+// 2. (+) nilai variabel bisa di timpa/ubah
+
+// const:
+// 1. (-) nama variabel tidak bisa di deklarasi ulang
+// 2. (-) nilai variabel tidak bisa di timpa/ubah
+    
+const peserta = 'Dul'
+peserta = 'iqbal'
+console.log(peserta)
