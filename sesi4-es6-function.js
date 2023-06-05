05ddb4ccf56fd71a0504136d41edefb67afbf8ef
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2023-06-05 21:20:59 +0700

    sesi4: es6

---
+++ sesi4-es6-function.js
@@ -0,0 +1,19 @@
+//cara penulisan tradisional
+// function tambah(gaji, lembur) {
+//     return gaji + lembur
+// }
+
+//cara penulisan versi baru atau es6
+// const tambah = (gaji, lembur) => gaji + lembur
+
+//penulisan function di dalam variabel versi tradisional
+const perkenalan = function () {
+    console.log('halo, nama saya aji')
+}
+
+//penulisan function di dalam variabel versi es6
+const salam = () => {
+    console.log('halo, nama saya aji')
+}
+salam()
+console.log('halo, nama saya drajad')
---
+++ sesi4-es6-looping.js
@@ -0,0 +1,9 @@
+var peserta = ['dul','nur','drajad','iqbal']
+
+for (let i = 0; i < peserta.length; i++) {
+    console.log( peserta[i] )
+}
+console.log( '--------------' )
+peserta.forEach(psrt => {
+    console.log( psrt )
+})
